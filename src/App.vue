f9ebc80c883f66cbbd953c031f84682ef2b2a1fe
Bjorkbat <bjorkbat@gmail.com>
2018-03-04 23:35:02 -0700

    So close

--- src/App.vue
+++ src/App.vue
@@ -180,6 +180,55 @@ export default {
 
     buildPaths: function(paths) {
 
+      // This is the formula for a path command.  Every path begins with a
+      // letter, followed by at least one positive or negative digit which may
+      // or may not have a decimal.  In some instances there's more than one
+      // "point", in which case additional numbers may follow after a comma,
+      // space, or negative sign as their seperator.
+      let pathCommandRegex = /([a-zA-Z]\-?\d+(\.\d+)?(([\s\,\-])*\d+(\.\d+)?)*)/g;
+
+      // Possible one of the hardest, as this entails a lot of regexp parsing
+      for(var i = 0; i < paths.length; i ++) {
+
+        // First, we only want the actual path data, so let's get that.
+        let pathRegExp = paths[i].match(/d="([^"]*)"/);
+        let pathData = "";
+        if (pathRegExp.length >= 2) {
+          pathData = pathRegExp[1];
+        }
+        console.log(pathData);
+
+        // Then, we're going to grab every single substring that begins with
+        // a letter and contains an unlimited number of digits and commas.
+        // The delimiter for every path command is going to be a letter
+        let pathCommands = pathData.match(pathCommandRegex);
+        console.log(pathCommands)
+
+        // Now, let's go ahead and create a new Threejs shape and analyze each
+        // point command individually to turn it into a command we can apply
+        // to our shape
+        let shape = new THREE.Shape();
+        shape.autoClose = true;
+        for(var j = 0; j < pathCommands.length; j ++) {
+
+          this.shapeSwitchCommand(shape, pathCommands[j]);
+
+        }
+
+        // After all of the above is said and done, you'll have your very own
+        // shape.  Now to turn it into a geometry
+        let geometry = new THREE.ExtrudeGeometry(shape, {
+          amount: 5,
+          bevelSize: 0,
+          bevelThickness: 0
+        });
+        let material = new THREE.MeshBasicMaterial( {color: 0xFF0000} );
+        let mesh = new THREE.Mesh(geometry, material);
+
+        this.objects.push(mesh);
+        this.object.add(mesh);
+      }
+
     },
 
     buildPolygons: function(polygons) {
@@ -288,12 +337,17 @@ export default {
         this.buildPolygons(polygons);
       }
 
+      // And finally, the tricky paths
+      if (paths != null) {
+        this.buildPaths(paths);
+      }
+
       this.parsed = true;
     },
 
     render: function() {
       requestAnimationFrame( this.render );
-      this.object.rotation.y += 0.005;
+      //this.object.rotation.y += 0.005;
       this.renderer.render(this.scene, this.camera);
     },
 
@@ -301,6 +355,150 @@ export default {
       const buffer = ObjExporter.meshes(this.objects).then(function(blob) {
         saveAs(blob, 'svg-to-three.zip');
       });
+    },
+
+    /**
+     * Takes some SVG path command string, breaks it down, then uses a switch to
+     * figure out what to do next.
+     */
+    shapeSwitchCommand: function(shape, command) {
+
+      // Each command begins with a letter.  Analyze the letter to
+      // understand the command.  While you're at it, get the points too.
+      let letter = command.match(/([a-zA-Z])((.)*)/);
+      let points = letter[2].split(/[\s\,]/);
+      console.log(command);
+      console.log(letter);
+
+      // Now for the fun
+      switch(letter[1]) {
+        case ("M"):
+          // Move to command.
+          if (points.length == 2) {
+            shape.moveTo(
+              parseFloat(points[0]) - this.originOffset.x,
+              this.originOffset.y - parseFloat(points[1])
+            );
+          }
+          break;
+        case ("m"):
+          // Relative move to.  We can use currentPoint to figure it out.
+          // IMPORTANT!: A relative increase in the Y in an SVG viewport is
+          // always interpreted as moving DOWN.  Easy fix is to simply subtract.
+          if (points.length == 2) {
+            shape.moveTo(
+              shape.currentPoint.x + parseFloat(points[0]),
+              shape.currentPoint.y - parseFloat(points[1])
+            );
+          }
+          break;
+        case ("L"):
+          // Line to
+          if (points.length == 2) {
+            shape.lineTo(
+              parseFloat(points[0]) - this.originOffset.x,
+              this.originOffset.y - parseFloat(points[1])
+            );
+          }
+          break;
+        case ("l"):
+          // Relative line to.  Again, use current point.
+          if (points.length == 2) {
+            shape.lineTo(
+              shape.currentPoint.x + parseFloat(points[0]),
+              shape.currentPoint.y - parseFloat(points[1])
+            );
+          }
+          break;
+        case ("H"):
+          // Horizontal line.  Use line to again, and just pass in
+          // currentPoint.y as second arg
+          if (points.length == 1) {
+            shape.lineTo(
+              parseFloat(points[0]) - this.originOffset.x,
+              shape.currentPoint.y
+            );
+          }
+          break;
+        case ("h"):
+          // Relative horizontal line.
+          if (points.length == 1) {
+            shape.lineTo(
+              shape.currentPoint.x + parseFloat(points[0]),
+              shape.currentPoint.y
+            );
+          }
+          break;
+        case ("V"):
+          // Vertical line.  See horizontal line.
+          if (points.length == 1) {
+            shape.lineTo(
+              shape.currentPoint.x,
+              this.originOffset.y - parseFloat(points[0])
+            );
+          }
+          break;
+        case ("v"):
+          // Relative vertical line
+          if (points.length == 1) {
+            shape.lineTo(
+              shape.currentPoint.x,
+              shape.currentPoint.y - parseFloat(points[0])
+            );
+          }
+          break;
+        case ("A"):
+          // An arc command.  Shape has arc and absolute arc commands. but
+          // they require slightly different args.  Using some other asshole's
+          // code
+          // https://github.com/mrdoob/three.js/blob/master/examples/webgl_geometry_extrude_shapes2.html
+          let rx = parseFloat(points[0]);
+          let ry = parseFloat(points[1]);
+          let xar = parseFloat(points[2]);
+          let laf = parseInt(points[3]);
+          let sf = parseInt(points[4]);
+          let nx = parseFloat(points[5]) - this.originOffset.x;
+          let ny = this.originOffset.y - parseFloat(points[6]);
+
+          let x = shape.currentPoint.x;
+          let y = shape.currentPoint.y;
+
+          let x1 = Math.cos( xar ) * ( x - nx ) / 2 + Math.sin( xar ) * ( y - ny ) / 2;
+          let y1 = - Math.sin( xar ) * ( x - nx ) / 2 + Math.cos( xar ) * ( y - ny ) / 2;
+
+          // Fuck this shit
+          let norm = Math.sqrt( ( rx * rx * ry * ry - rx * rx * y1 * y1 - ry * ry * x1 * x1 ) /
+									 ( rx * rx * y1 * y1 + ry * ry * x1 * x1 ) );
+
+          // This was causing weird bullshit.  Had to nuke it.
+          //if ( laf === sf ) norm = - norm;
+
+          let x2 = norm * rx * y1 / ry;
+					let y2 = norm * -ry * x1 / rx;
+
+          let cx = Math.cos( xar ) * x2 - Math.sin( xar ) * y2 + ( x + nx ) / 2;
+				  let cy = Math.sin( xar ) * x2 + Math.cos( xar ) * y2 + ( y + ny ) / 2;
+
+          let u = new THREE.Vector2( 1, 0 );
+					let v = new THREE.Vector2( ( x1 - x2 ) / rx, ( y1 - y2 ) / ry );
+
+          let startAng = Math.acos( u.dot( v ) / u.length() / v.length() );
+
+          if ( ( ( u.x * v.y ) - ( u.y * v.x ) ) < 0 ) startAng = - startAng;
+
+          u.x = ( - x1 - x2 ) / rx;
+					u.y = ( - y1 - y2 ) / ry;
+
+          let deltaAng = Math.acos( v.dot( u ) / v.length() / u.length() );
+
+          if ( ( ( v.x * u.y ) - ( v.y * u.x ) ) < 0 ) deltaAng = - deltaAng;
+					if ( ! sf && deltaAng > 0 ) deltaAng -= Math.PI * 2;
+					if ( sf && deltaAng < 0 ) deltaAng += Math.PI * 2;
+
+          shape.absarc(cx, cy, rx, startAng, startAng + deltaAng, sf);
+          break;
+
+      }
     }
 
   },
